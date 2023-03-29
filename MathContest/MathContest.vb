6d6cbe13700c9be41f7c047ed8ac2e4af8556849
Isabella <belladougherty@gmail.com>
2023-03-29 08:32:12 -0600

    Made it so the user can leave the grade and age text boxes when the box is empty and began work on the random number section.

--- MathContest/MathContest.vb
+++ MathContest/MathContest.vb
@@ -7,17 +7,15 @@
 
 Public Class MathContest
     Dim studentsName As String = Nothing
+    Dim answer As String
     Dim studentsAge As Integer = Nothing
     Dim studentsGrade As Integer = Nothing
-    Dim showMessage As Boolean = True
-    Dim MessageBoxShown As Boolean
-    Dim happened As Boolean
-    Dim random As New Random()
     Dim first As Integer
     Dim second As Integer
     Dim correct As Integer
-    Dim answer As String
     Dim answeredCorrectly As Boolean
+    Dim happened As Boolean
+    Dim random As New Random()
 
     'Checks if the input name is empty or not. If it is not, turns the viable boolean true to be used later and makes the
     'clear button usable
@@ -34,12 +32,17 @@ Public Class MathContest
         Else
             ClearButton.Enabled = True
         End If
-
         Try
             studentsAge = CInt(AgeTextBox.Text)
+            If studentsAge < 7 Or studentsAge > 11 Then
+                MsgBox("Student not eligable to compete")
+            End If
         Catch ex As Exception
-            MsgBox("You must input the age in numbers not letters.")
-            AgeTextBox.Focus()
+            If AgeTextBox.Text <> "" Then
+                MsgBox("You must input the age (numbers not letters.)")
+                AgeTextBox.Text = ""
+                AgeTextBox.Focus()
+            End If
         End Try
     End Sub
 
@@ -52,16 +55,43 @@ Public Class MathContest
 
         Try
             studentsGrade = CInt(GradeTextBox.Text)
+            If studentsGrade < 1 Or studentsGrade > 4 Then
+                MsgBox("Student not eligable to compete")
+            End If
         Catch ex As Exception
-            MsgBox("You must input the grade in numbers not letters.")
-            GradeTextBox.Focus()
+            If GradeTextBox.Text <> "" Then
+                MsgBox("You must input the grade (numbers not letters.)")
+                GradeTextBox.Text = ""
+                GradeTextBox.Focus()
+            End If
         End Try
+    End Sub
+
+    Private Sub NumberGen()
+        If NameTextBox.Text <> "" And AgeTextBox.Text <> "" And GradeTextBox.Text <> "" Then
+            first = random.Next(1, 11)
+            second = random.Next(1, 11)
+
+            If AdditionRadioButton.Checked = True Then
+                correct = first + second
+            ElseIf SubtractionRadioButton.Checked = True Then
+                correct = first - second
+            ElseIf MultiplicationRadioButton.Checked = True Then
+                correct = first * second
+            ElseIf DivisionRadioButton.Checked = True Then
+                correct = first / second
+            Else
+                MsgBox("You must select a function.")
+            End If
+
+        End If
     End Sub
 
     'Checks for if any of the information inputs are empty. If one is the user is alerted they must fill that
     Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
 
     End Sub
+
     'Clears every variable and textbox
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         studentsName = ""
@@ -76,7 +106,6 @@ Public Class MathContest
         SubmitButton.Enabled = False
         SummeryButton.Enabled = False
     End Sub
-
 
     'Checks for if any of the information textboxes are empty. If one is defaults back to the addition radio button. If not the radio
     'button is allowed to change
